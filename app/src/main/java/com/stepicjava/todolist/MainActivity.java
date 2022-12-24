958d2695927c9c7c87c182c140b0e135f770b0c6
alexpurs1980 <alexpurs@yandex.ru>
2022-12-24 19:12:17 +0300

    initViews

--- app/src/main/java/com/stepicjava/todolist/MainActivity.java
+++ app/src/main/java/com/stepicjava/todolist/MainActivity.java
@@ -3,12 +3,24 @@ package com.stepicjava.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews(){
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        buttonAddNote = findViewById(R.id.floatingActionButtonAddNote);
     }
 }
