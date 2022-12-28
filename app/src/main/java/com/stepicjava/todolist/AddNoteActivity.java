d9b0fe5f1ff5c8cb8c3cb2d96ee6050b1b8c479a
alexpurs1980 <alexpurs@yandex.ru>
2022-12-28 10:26:30 +0300

    initViews Add notes, views with notes Add new activity AddNote

--- app/src/main/java/com/stepicjava/todolist/AddNoteActivity.java
+++ app/src/main/java/com/stepicjava/todolist/AddNoteActivity.java
@@ -3,13 +3,55 @@ package com.stepicjava.todolist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
+    private RadioButton radioButtonLow;
+    private RadioButton radioButtonMedium;
+    private RadioButton radioButtonHigh;
+    private Button buttonSaveNote;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        initViews();
+        buttonSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveNote();
+            }
+        });
+    }
+
+    private void initViews(){
+        editTextNote = findViewById(R.id.editTextNote);
+        radioButtonLow = findViewById(R.id.radioButtonLow);
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);
+        radioButtonHigh = findViewById(R.id.radioButtonHigh);
+        buttonSaveNote = findViewById(R.id.buttonSaveNote);
+
+    }
+
+    private void saveNote(){
+        String text = editTextNote.getText().toString().trim();
+    }
+
+    private void getPriority(){
+        int priority;
+        if (radioButtonLow.isChecked()) {
+            priority = 0;
+        } else if (radioButtonMedium.isChecked()) {
+            priority = 1;
+        } else {
+            priority = 2;
+        }
     }
 
 
